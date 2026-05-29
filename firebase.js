fb7197c7e00af7c33e7cff27043f0e0ae87f9cec
inharmonyy-star <inharmonyy@gmail.com>
2026-05-29 16:12:02 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDXrqL5DdtFNpPTphcij-PP0fcQccU-_jA",
   authDomain: "fifa-dashboard-b3a3f.firebaseapp.com",
   projectId: "fifa-dashboard-b3a3f",
-  storageBucket: "fifa-dashboard-b3a3f.firebasestorage.app",
+  storageBucket: "fifa-dashboard-b3a3f.appspot.com",
   messagingSenderId: "1069326385461",
   appId: "1:1069326385461:web:c6abbd0e3500c3fd584c7c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
